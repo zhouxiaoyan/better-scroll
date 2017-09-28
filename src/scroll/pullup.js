718a58b9bd031f2b01f2c97e7ac7c49120c62fbd
zhouxiaoyan <609519669@qq.com>
2017-09-28 21:35:23 +0800

    Update pullup.js

--- src/scroll/pullup.js
+++ src/scroll/pullup.js
@@ -1,3 +1,4 @@
+//这个配置用于做上拉加载功能
 export function pullUpMixin(BScroll) {
   BScroll.prototype._initPullUp = function () {
     // must watch scroll in real time
@@ -6,21 +7,22 @@ export function pullUpMixin(BScroll) {
     this.pullupWatching = false
     this._watchPullUp()
   }
-
+//这个函数里绑定scroll事件。并且被调用。绑定事件也是在特定的事件绑定所以放在一个函数中。
   BScroll.prototype._watchPullUp = function () {
     if (this.pullupWatching) {
       return
     }
     this.pullupWatching = true
     const {threshold = 0} = this.options.pullUpLoad
-
+//监听scroll事件，检查是不是到底了
     this.on('scroll', checkToEnd)
-
+//checkToend事件传入的事件是一个对象如{x:0;y:-300}；为什么？threshold顶部下拉的距离
     function checkToEnd(pos) {
       if (this.movingDirectionY === 1 && pos.y <= (this.maxScrollY + threshold)) {
         this.trigger('pullingUp')
+        //触发pullingUp事件。这里没有定义返回。
         this.pullupWatching = false
-        this.off('scroll', checkToEnd)
+        this.off('scroll', checkToEnd)//解绑
       }
     }
   }
